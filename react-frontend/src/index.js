0c14b52f852915322fd38142b4076227a4c1e11b
Prem Singh <PremSingh.Official29@gmail.com>
2024-07-14 23:32:44 +0530

    feat: wrap the app with context for improved state management

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { WeatherContextProvider } from "./context/weather";
+import { ToastContextProvider } from "./context/toast";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WeatherContextProvider>
+      <ToastContextProvider>
+        <App />
+      </ToastContextProvider>
+    </WeatherContextProvider>
   </React.StrictMode>
 );
