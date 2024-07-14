4b4ae697b2d8610818c4087cec4bdf3f1a3b8f66
Prem Singh <PremSingh.Official29@gmail.com>
2024-07-14 23:23:36 +0530

    feat: create index file to export components directly

---
+++ react-frontend/src/components/index.js
@@ -0,0 +1,11 @@
+export { default as BackgroundLayout } from "./backgroundLayout";
+
+export { default as NavBar } from "./navbar";
+
+export { default as WeatherCard } from "./weatherCard";
+
+export { default as MiniCard } from "./miniCard";
+
+export { default as WeatherChart } from "./weatherChart";
+
+export { default as Toast } from "./toast";
