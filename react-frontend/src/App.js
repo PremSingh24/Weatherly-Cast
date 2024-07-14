bb6f5efd37920cef7ee0052dcd8223768f1f051c
Prem Singh <PremSingh.Official29@gmail.com>
2024-07-14 23:32:03 +0530

    feat: add routes to manage navigation within the application

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BackgroundLayout, NavBar } from "./components";
+import WeatherPage from "./pages/weatherPage";
+import LoginPage from "./pages/loginPage";
+import RegisterPage from "./pages/registerPage";
+import Toast from "./components/toast";
+
 function App() {
-  return <div className="App">Hey this is new</div>;
+  return (
+    <BrowserRouter>
+      <BackgroundLayout>
+        <NavBar />
+        <Routes>
+          <Route index element={<WeatherPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+        <Toast />
+      </BackgroundLayout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
