fe1a957b026bc26f62f4cbc969f19ab2ed0e0052
Prem Singh <PremSingh.Official29@gmail.com>
2024-07-28 23:16:06 +0530

    feat: add city context provider to wrap the app

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { WeatherContextProvider } from "./context/weather";
 import { ToastContextProvider } from "./context/toast";
+import { CityContextProvider } from "./context/city";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <WeatherContextProvider>
       <ToastContextProvider>
-        <App />
+        <CityContextProvider>
+          <App />
+        </CityContextProvider>
       </ToastContextProvider>
     </WeatherContextProvider>
   </React.StrictMode>
