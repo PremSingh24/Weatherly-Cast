3a10921d07baf5406ba91c629a2439961419696a
Prem Singh <PremSingh.Official29@gmail.com>
2024-07-29 00:00:50 +0530

    chore: remove unnecessary console logs

--- react-frontend/src/services/authServices/register.service.js
+++ react-frontend/src/services/authServices/register.service.js
@@ -1,5 +1,4 @@
 export const registerUserService = async ({ username, password }) => {
-  console.log("username ", username, " password ", password);
   try {
     const response = await fetch(
       `${process.env.REACT_APP_BASE_URL}/api/v1/auth/register`,
