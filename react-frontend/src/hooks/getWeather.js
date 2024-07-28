5bd4eaeab58d9c307e9a02d4d40409c3533bc25b
Prem Singh <PremSingh.Official29@gmail.com>
2024-07-29 00:01:42 +0530

    feat: add error handling and toast message for weather fetch failures

--- react-frontend/src/hooks/getWeather.js
+++ react-frontend/src/hooks/getWeather.js
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useWeatherContext } from "../context/weather";
 import getWeather from "../api/getWeather";
+import { useToastContext } from "../context/toast";
 
 const useGetWeather = () => {
   const { setWeather, setLocation } = useWeatherContext();
+  const { setSeverity, setMessage, setOpen } = useToastContext();
 
   useEffect(() => {
     (async () => {
@@ -12,9 +14,19 @@ const useGetWeather = () => {
         : "Delhi, India";
       const weatherResult = await getWeather(location);
 
-      setLocation(location);
-
-      setWeather(weatherResult.values);
+      if (weatherResult?.status) {
+        const res = await weatherResult.json();
+        setSeverity("error");
+        if (res?.message) {
+          setMessage(res.message.slice(0, 69));
+        } else {
+          setMessage("Can not fetch weather at this moment");
+        }
+        setOpen(true);
+      } else {
+        setLocation(location);
+        setWeather(weatherResult.values);
+      }
     })();
   }, []);
 };
