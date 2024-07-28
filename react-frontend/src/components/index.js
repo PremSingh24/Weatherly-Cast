17424a4fe249cd05dbf4e87b72f446b097da15e5
Prem Singh <PremSingh.Official29@gmail.com>
2024-07-28 23:05:40 +0530

    feat: add default export of SearchBox component

--- react-frontend/src/components/index.js
+++ react-frontend/src/components/index.js
@@ -1,11 +1,19 @@
 export { default as BackgroundLayout } from "./backgroundLayout";
 
-export { default as NavBar } from "./navbar";
+export { default as FavouriteCityList } from "./favouriteCityList";
 
-export { default as WeatherCard } from "./weatherCard";
+export { default as Loader } from "./loader";
 
 export { default as MiniCard } from "./miniCard";
 
-export { default as WeatherChart } from "./weatherChart";
+export { default as NavBar } from "./navbar";
+
+export { default as SearchBox } from "./searchBox";
+
+export { default as StarButton } from "./starButton";
 
 export { default as Toast } from "./toast";
+
+export { default as WeatherCard } from "./weatherCard";
+
+export { default as WeatherChart } from "./weatherChart";
