2fe0fa386428aa13552491dbc3340bd4ba2917dd
Prem Singh <PremSingh.Official29@gmail.com>
2024-08-08 21:54:57 +0530

    perf: implement image preloading for better performance

--- react-frontend/src/components/backgroundLayout.jsx
+++ react-frontend/src/components/backgroundLayout.jsx
@@ -1,11 +1,11 @@
 import { Box } from "@mui/material";
-import clear from "../assets/Clear.jpg";
-import cloudy from "../assets/Cloudy.jpg";
-import fog from "../assets/fog.png";
-import rainy from "../assets/Rainy.jpg";
-import stormy from "../assets/Stormy.jpg";
-import sunny from "../assets/Sunny.jpg";
-import snow from "../assets/snow.jpg";
+import clear from "../assets/Clear.webp";
+import cloudy from "../assets/Cloudy.webp";
+import fog from "../assets/fog.webp";
+import rainy from "../assets/Rainy.webp";
+import stormy from "../assets/Stormy.webp";
+import sunny from "../assets/Sunny.webp";
+import snow from "../assets/snow.webp";
 import { useEffect, useState } from "react";
 import { useWeatherContext } from "../context/weather";
 
@@ -14,22 +14,33 @@ const BackgroundLayout = ({ children }) => {
   const [image, setImage] = useState(sunny);
 
   useEffect(() => {
+    const preloadImages = (images) => {
+      images.forEach((image) => {
+        const img = new Image();
+        img.src = image;
+      });
+    };
+
+    preloadImages([clear, cloudy, fog, rainy, stormy, sunny, snow]);
+  }, []);
+
+  useEffect(() => {
     if (weather && weather[0]?.conditions) {
       let imageString = weather[0]?.conditions;
-      if (imageString.toLowerCase().includes("clear")) {
-        setImage(clear);
-      } else if (imageString.toLowerCase().includes("sunny")) {
+      if (imageString.toLowerCase().includes("sunny")) {
         setImage(sunny);
-      } else if (
-        imageString.toLowerCase().includes("cloud") ||
-        imageString.toLowerCase().includes("overcast")
-      ) {
-        setImage(cloudy);
+      } else if (imageString.toLowerCase().includes("clear")) {
+        setImage(clear);
       } else if (
         imageString.toLowerCase().includes("rain") ||
         imageString.toLowerCase().includes("shower")
       ) {
         setImage(rainy);
+      } else if (
+        imageString.toLowerCase().includes("cloud") ||
+        imageString.toLowerCase().includes("overcast")
+      ) {
+        setImage(cloudy);
       } else if (imageString.toLowerCase().includes("snow")) {
         setImage(snow);
       } else if (imageString.toLowerCase().includes("fog")) {
@@ -52,6 +63,7 @@ const BackgroundLayout = ({ children }) => {
         backgroundImage: `url(${image})`,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
+        transition: "background-image 0.5s ease-in-out",
       }}
     >
       {children}
